bacc8bf78bf0d5f5585579b501b8689e9f21d8c5
lmynsberge <lmynsberge@gmail.com>
2017-12-28 09:37:25 -0600

    Add search results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-  <h1>{{title}}</h1>
-  <nav>
-    <a routerLink="/dashboard">Dashboard</a> |
-    <a routerLink="/flights">Flights</a> |
-    <a routerLink="/results">Results</a>
-  </nav>
-  <router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 
 export class AppComponent {
